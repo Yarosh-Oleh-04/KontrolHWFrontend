5472c5c33194bdfa1801f63fd9d1c9a2685f49b4
Yarosh-Oleh-04 <yarosholeh04@gmail.com>
2019-01-10 22:16:16 +0200

    add face-slider

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
-// Helper slider: create cloneNode
+// Helper slider:
 
 function helper_slider(n){
     var slider = document.getElementById('helper-slides')
@@ -13,4 +13,50 @@ function helper_slider(n){
         document.getElementsByClassName('others-dot')[n].classList.add('active')
         slider.style.left = -n * size + 'vw'
     
+}
+
+//Face slider: cloneNode
+
+var clone1 = document.getElementsByClassName('face-slide')[0].cloneNode(true)
+var clone2 = document.getElementsByClassName('face-slide')[1].cloneNode(true)
+var clone3 = document.getElementsByClassName('face-slide')[2].cloneNode(true)
+var clone4 = document.getElementsByClassName('face-slide')[0].cloneNode(true)
+var clone5 = document.getElementsByClassName('face-slide')[2].cloneNode(true)
+
+clone4.classList.add('clone')
+clone5.classList.add('clone')
+
+document.getElementsByClassName('face-slides')[0].innerHTML = ''
+document.getElementsByClassName('face-slides')[0].appendChild(clone5)
+document.getElementsByClassName('face-slides')[0].appendChild(clone1)
+document.getElementsByClassName('face-slides')[0].appendChild(clone2)
+document.getElementsByClassName('face-slides')[0].appendChild(clone3)
+document.getElementsByClassName('face-slides')[0].appendChild(clone4)
+
+var left = -200
+function face_slider(n){
+    slider = document.getElementsByClassName('face-slides')[0]
+    slider.style.transition = 'all .5s ease'
+    if (n == 0){left += 100}
+    if (n == 1){left -= 100}
+
+    if (left == -400){
+        slider.style.transition = ''
+        slider.style.left = '0vw'
+        setTimeout(function(){
+            slider.style.transition = 'all .5s ease'
+            left = -100
+            slider.style.left = left + 'vw'
+        },10)
+    } else if(left == 0){
+        slider.style.transition = ''
+        slider.style.left = '-400vw'
+        setTimeout(function(){
+            slider.style.transition = 'all .5s ease'
+            left = -300
+            slider.style.left = left + 'vw'
+        },10)
+    } else {
+        slider.style.left = left + 'vw'
+    }
 }
